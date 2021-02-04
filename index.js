119560baaea1f9873e1751e41f5af19d3ec3e696
brian4548 <brianr4548@gmail.com>
2021-02-04 11:30:20 -0600

    commit tree bd1b2e30e56d6303538daa11cfea5a1a03f1bc71

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  if (time < "12:00") {
+    return "Good Morning";
+  } else if (time >= "12:00" && time < "17:00") {
+      return "Good Afternoon";
+  } else if (time > "17:00" && time < "24:00") {
+      return "Good Evening";
+  }
+}
+function displayMessage(content) {
+  let greeting = document.getElementById('greeting')
+  greeting.innerText = content
+}
